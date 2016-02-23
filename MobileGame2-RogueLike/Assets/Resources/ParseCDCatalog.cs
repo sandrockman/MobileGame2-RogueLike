b3863ed9b891fb24733c518140fc9063c6ce7211
sandrockman <svc-student@RC335G03.student.directory.jccc>
2016-02-23 15:11:34 -0600

    finished xml parse homework

--- MobileGame2-RogueLike/Assets/Resources/ParseCDCatalog.cs
+++ MobileGame2-RogueLike/Assets/Resources/ParseCDCatalog.cs
@@ -8,7 +8,7 @@ public class ParseCDCatalog : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        TextAsset xmlFile = Resources.Load("productListing") as TextAsset;
+        TextAsset xmlFile = Resources.Load("CDCatalog") as TextAsset;
 
         XmlDocument xmlDoc = new XmlDocument();
         //3 types of reading xml docs.
@@ -27,50 +27,56 @@ public class ParseCDCatalog : MonoBehaviour {
 
         foreach (XmlNode node in xmlDoc.DocumentElement)
         {
-            int cdYear = 0;
-            string title = "t", artist = "a", country = "cu", company = "cm";
+            //Debug.Log(node.Name);
 
-            //output the information into the debug
-            foreach (XmlNode node2 in node.ChildNodes)
-            {
-                //if year of CD is after 1980, Output the Title/Artist
-                //if year of CD is after 1990, Output the Title/Artist/Country/Company
-                //Don't output anything if CD year is 2000 or later
-                //Debug.Log(node.Name + " - " + node2.Name + " - " + node2.InnerText);
-                if (node2.Name == "TITLE")
-                {
-                    title = node2.InnerText;
-                }
-                if (node2.Name == "ARTIST")
-                {
-                    artist = node2.InnerText;
-                }
-                if (node2.Name == "COUNTRY")
-                {
-                    country = node2.InnerText;
-                }
-                if (node2.Name == "COMPANY")
-                {
-                    company = node2.InnerText;
-                }
-                if (node2.Name == "YEAR")
-                {
-                    cdYear = Convert.ToInt32(node.LastChild.InnerText);
-                }
+            //foreach (XmlNode node in file.ChildNodes)
+            //{
+                int cdYear = 0;
+                string title = "t", artist = "a", country = "cu", company = "cm";
 
-            }
-            if(cdYear < 2000)
-            {
-                if(cdYear > 1990)
+                //output the information into the debug
+                foreach (XmlNode node2 in node.ChildNodes)
                 {
-                    Debug.Log(title + ", " + artist + ", " + country + ", " + company);
-                }
-                else if(cdYear > 1980)
-                {
-                    Debug.Log(title + ", " + artist);
+                    //if year of CD is after 1980, Output the Title/Artist
+                    //if year of CD is after 1990, Output the Title/Artist/Country/Company
+                    //Don't output anything if CD year is 2000 or later
+                    //Debug.Log(node.Name + " - " + node2.Name + " - " + node2.InnerText);
+                    if (node2.Name == "TITLE")
+                    {
+                        title = node2.InnerText;
+                    }
+                    if (node2.Name == "ARTIST")
+                    {
+                        artist = node2.InnerText;
+                    }
+                    if (node2.Name == "COUNTRY")
+                    {
+                        country = node2.InnerText;
+                    }
+                    if (node2.Name == "COMPANY")
+                    {
+                        company = node2.InnerText;
+                    }
+                    if (node2.Name == "YEAR")
+                    {
+                        cdYear = Convert.ToInt32(node.LastChild.InnerText);
+                    }
 
+
+                    if (cdYear < 2000)
+                    {
+                        if (cdYear > 1990)
+                        {
+                            Debug.Log(title + ", " + artist + ", " + country + ", " + company);
+                        }
+                        else if (cdYear > 1980)
+                        {
+                            Debug.Log(title + ", " + artist);
+
+                        }
+                    }
                 }
-            }
+            //*/
         }
 
         /*
